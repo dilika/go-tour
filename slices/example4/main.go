19f6ea1f6b07ae2e8174b67cca51191f3c5ade93
dilika <modiboshs@gmail.com>
2026-07-21 09:47:35 +0000

    Slice example reproduction in progress...

--- slices/example4/main.go
+++ slices/example4/main.go
@@ -2,7 +2,6 @@ package main
 
 import from "fmt"
 
-
 func main() {
 
 	// Create a slice with a length of 5 elements and a capacity of 8.
@@ -15,25 +14,24 @@ func main() {
 
 	inspectSlice(fruits)
 
-
-// Take a slice of slice1. We want just indexes 2 and 3.
+	// Take a slice of slice1. We want just indexes 2 and 3.
 	// Parameters are [starting_inudex : (starting_index + length)]
 	fruits1 := fruits[2:4]
 	inspectSlice(fruits1)
 
-fmt.Println("=========================================================")
-// Change the value of the index 0 of slice2.
- fruits1[0] = "Tomatoe"
+	fmt.Println("=========================================================")
+	// Change the value of the index 0 of slice2.
+	fruits1[0] = "Tomatoe"
 
-		// Display the change across all existing slices.
-		inspectSlice(fruits)
-		inspectSlice(fruits1)
+	// Display the change across all existing slices.
+	inspectSlice(fruits)
+	inspectSlice(fruits1)
 
-		// Make a new slice big enough to hold elements of slice 1 and copy the
-		// values over using the builtin copy function.
-		newFruits := make([]string, len(fruits))
-		copy(newFruits, fruits)
-		inspectSlice(newFruits)
+	// Make a new slice big enough to hold elements of slice 1 and copy the
+	// values over using the builtin copy function.
+	newFruits := make([]string, len(fruits))
+	copy(newFruits, fruits)
+	inspectSlice(newFruits)
 
 	// inspectSlice exposes the slice header for review.
 
