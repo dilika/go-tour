38aae01b9842605a0d9efccb6ba0f8c0577fe747
dilika <modiboshs@gmail.com>
2026-07-29 00:53:29 +0000

    Example 9 reproduced

---
+++ slices/example8/main.go
@@ -0,0 +1,24 @@
+package main 
+
+
+import "fmt"
+
+func main(){
+// Using the value semantic form of the for range.
+ friends := []string{"Hardin", "Lewis", "Manlio", "Pirenne", "Dornick", "Sutt"}
+ for _, v := range friends {
+	 friends = friends[:2]
+	 fmt.Printf("\nHere is the friend : [ %v ] \t ", v)
+ }
+
+
+
+
+ friendss := []string{"Hardin", "Lewis", "Manlio", "Pirenne", "Dornick", "Sutt"}
+	for i := range friendss {
+		friendss = friendss[:2]
+		fmt.Printf("\n Here is friend [%v]", friends[i])
+	}
+
+	// Using the pointer semantic form of the for range.
+}
